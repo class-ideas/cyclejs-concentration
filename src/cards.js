f029defd1edff76e9c2f6f2abadf0bd5a34cddcc
JD Isaacks <jd@jisaacks.com>
2015-12-11 16:04:31 -0500

    split functionality into model view intent

--- src/cards.js
+++ src/cards.js
@@ -1,9 +1,64 @@
 /** @jsx hJSX */
 
 import {h, hJSX} from '@cycle/dom';
+import Rx from 'rx';
 import classes from 'classnames';
+import deck from './deck';
 
-function view(cards$) {
+function intent(DOM) {
+  return {
+    flip$: DOM
+      .select('.card')
+      .events('click')
+      .map(e => {
+        let id   = e.currentTarget.getAttribute('data-id');
+        let suit = e.currentTarget.getAttribute('data-suit');
+        let face = e.currentTarget.getAttribute('data-face');
+        return {id, face, suit};
+      })
+  }
+}
+
+function model(actions, {suits$}) {
+  let cards$ = suits$.map(n => deck(n));
+
+  let [odd$, even$] = actions.flip$
+    .partition((card, index) => index % 2 === 0)
+  
+  let oddPairs$ = odd$.map(odd => [odd, {}]);
+  let evenPairs$ = even$
+    .withLatestFrom(odd$, (even, odd) => odd.id === even.id ? [{},{}] : [odd, even])
+  
+  let pairs$ = Rx.Observable
+    .merge(oddPairs$, evenPairs$)
+    .startWith([{}, {}]);
+
+  let matches$ = evenPairs$
+    .filter(([a, b]) => (
+      a.face === b.face && a.suit === b.suit
+    ))
+    .do(([a, b]) => console.log('MATCH!', a, b))
+    .scan((set, [a, b]) => {
+      set.add(a.id);
+      set.add(b.id);
+      return set;
+    }, new Set())
+    .startWith(new Set())
+    .do(m => console.log('matches:', m))
+
+  return pairs$
+    .combineLatest(matches$, cards$, 
+      ([a, b], matches, cards) => {
+        cards.forEach(card => {
+          card.shown = (card.id === a.id || card.id === b.id);
+          card.match = matches.has(card.id);
+        });
+        return cards;
+      }
+    );
+}
+
+function view(state$) {
 
   let card = ({suit, face, shown, match, id}) => (
     <div className="card-slot">
@@ -19,7 +74,7 @@ function view(cards$) {
     </div>
   );
 
-  let vtree$ = cards$.map(cards => (
+  let vtree$ = state$.map(cards => (
     <div className="cards">{cards.map(card)}</div>
   ));
 
@@ -28,12 +83,12 @@ function view(cards$) {
   }
 }
 
-export default function card(cards$) {
+export default function cards({DOM, suits$}) {
 
-  let {vtree$} = view(cards$);
+  let {vtree$} = view(model(intent(DOM), {suits$}));
 
   return {
-    DOM: vtree$
+    vtree$
   };
 
 }
--- src/memory.js
+++ src/memory.js
@@ -1,62 +1,33 @@
-import {h} from '@cycle/dom';
-import Rx from 'rx';
+/** @jsx hJSX */
 
-import deck from './deck';
+import {h, hJSX} from '@cycle/dom';
+import Rx from 'rx';
 import Cards from './cards';
 
-function view(DOM) {
+function model(DOM) {
+  let suits$ = Rx.Observable.of(1);
+  let cards$ = Cards({DOM, suits$}).vtree$;
+  return Rx.Observable.combineLatest(suits$, cards$, 
+    (suits, cards) => {
+      return {suits, cards};
+    }
+  );
+}
 
-  let cards$ = Rx.Observable.of(deck(1)) // 1 - 4
+function view(state$) {
+  let layout = ({suits, cards}) => (
+    <div>
+      <h1>Memory</h1>
+      <div>Suits: {suits}</div>
+      {cards}
+    </div>
+  );
 
-  let flip$ = DOM
-    .select('.card')
-    .events('click')
-    .map(e => {
-      let id   = e.currentTarget.getAttribute('data-id');
-      let suit = e.currentTarget.getAttribute('data-suit');
-      let face = e.currentTarget.getAttribute('data-face');
-      return {id, face, suit};
-    });
-
-  let [odd$, even$] = flip$
-    .partition((card, index) => index % 2 === 0)
-  
-  let oddPairs$ = odd$.map(odd => [odd, {}]);
-  let evenPairs$ = even$
-    .withLatestFrom(odd$, (even, odd) => odd.id === even.id ? [{},{}] : [odd, even])
-  
-  let pairs$ = Rx.Observable
-    .merge(oddPairs$, evenPairs$)
-    .startWith([{}, {}]);
-
-  let matches$ = evenPairs$
-    .filter(([a, b]) => (
-      a.face === b.face && a.suit === b.suit
-    ))
-    .do(([a, b]) => console.log('MATCH!', a, b))
-    .scan((set, [a, b]) => {
-      set.add(a.id);
-      set.add(b.id);
-      return set;
-    }, new Set())
-    .startWith(new Set())
-    .do(m => console.log('matches:', m))
-
-  return Cards(
-    pairs$
-      .combineLatest(matches$, cards$, 
-        ([a, b], matches, cards) => {
-        cards.forEach(card => {
-          card.shown = (card.id === a.id || card.id === b.id);
-          card.match = matches.has(card.id);
-        });
-        return cards;
-      })
-  ).DOM;
+  return state$.map(layout);
 }
 
 export default function memory({DOM}) {
   return {
-    DOM: view(DOM)
+    DOM: view(model(DOM))
   };
 }
