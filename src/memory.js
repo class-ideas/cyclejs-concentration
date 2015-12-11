072258550bd4897c7155536ba7a1396a82b43cc6
JD Isaacks <jd@jisaacks.com>
2015-12-10 23:42:04 -0500

    Get pairs working as desired

--- src/memory.js
+++ src/memory.js
@@ -8,18 +8,34 @@ function view(DOM) {
 
   let cards$ = Rx.Observable.of(deck(4))
 
-  return Cards(DOM
+  let flip$ = DOM
     .select('.card')
     .events('click')
     .map(e => e.currentTarget.getAttribute('data-id'))
-    .do(c => console.log('card:', c))
-    .startWith(null)
-    .combineLatest(cards$, (id, cards) => {
-      cards.forEach(card => {
-        card.shown = (card.id === id);
-      });
-      return cards;
-    })).DOM;
+    .do(c => console.log('flip:', c))
+    // .startWith(null);
+
+  //
+  let [odd$, even$] = flip$.partition((card, index) => index % 2 === 0)
+  let oddPairs$ = odd$.map(card => [card, null]);
+  let evenPairs$ = even$.withLatestFrom(odd$, (even, odd) => [odd, even]);
+  let pairs$ = Rx.Observable.merge(oddPairs$, evenPairs$).startWith([null, null]);
+  // pairs$.subscribe(([odd, even]) => console.log('pair', odd, even));
+  //
+
+  let combos$ = flip$.bufferWithCount(2)
+    .startWith([null, null])
+    .do(c => console.log('combo', c));
+
+  return Cards(
+    pairs$
+      .combineLatest(cards$, ([aid, bid], cards) => {
+        cards.forEach(card => {
+          card.shown = (card.id === aid || card.id === bid);
+        });
+        return cards;
+      })
+  ).DOM;
 }
 
 export default function memory({DOM}) {
