5b1e85368a9ca5f5e37c66ef56134bdfa33ac70e
JD Isaacks <jd@jisaacks.com>
2015-12-11 21:29:02 -0500

    Allow choosing suits

--- src/components/main.js
+++ src/components/main.js
@@ -3,22 +3,24 @@
 import {h, hJSX} from '@cycle/dom';
 import Rx from 'rx';
 import Cards from './cards';
+import NewGame from './new_game';
 
 function model(DOM) {
-  let suits$ = Rx.Observable.of(1);
-  let cards$ = Cards({DOM, suits$}).vtree$;
-  return Rx.Observable.combineLatest(suits$, cards$, 
-    (suits, cards) => {
-      return {suits, cards};
+  let game = NewGame(DOM);
+  let cards = Cards({DOM, suits$: game.suits$});
+  return Rx.Observable
+    .combineLatest(game.vtree$, cards.vtree$.startWith(null), 
+    (menu, cards) => {
+      return {menu, cards};
     }
   );
 }
 
 function view(state$) {
-  let layout = ({suits, cards}) => (
+  let layout = ({menu, cards}) => (
     <div>
       <h1>Memory</h1>
-      <div>Suits: {suits}</div>
+      {menu}
       {cards}
     </div>
   );
---
+++ src/components/new_game.js
@@ -0,0 +1,41 @@
+/** @jsx hJSX */
+
+import {h, hJSX} from '@cycle/dom';
+import Rx from 'rx';
+
+function intent(DOM) {
+  let suits$ = DOM
+    .select('.suit-count-select')
+    .events('change')
+    .map(e => e.target.value)
+    .startWith(1);
+
+  let newGame$ = DOM
+    .select('.start-new-game')
+    .events('click')
+    .withLatestFrom(suits$, (e, suits) => suits);
+
+  return newGame$;
+}
+
+function view() {
+  let opt = val => (
+    <option key={val} value={val}>{val}</option>
+  );
+
+  return Rx.Observable.of(
+    <div>
+      <select className="suit-count-select">
+        {[1,2,3,4].map(opt)}
+      </select>
+      <button className="start-new-game">Start</button>
+    </div>
+  );
+}
+
+export default function newGame(DOM) {
+  return {
+    vtree$: view(),
+    suits$: intent(DOM)
+  };
+}
